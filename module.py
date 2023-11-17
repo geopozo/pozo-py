a2f3ea3c39b2dc177e3623911261c2dd22fe95ce
Andrew Pikul <ajpikul@gmail.com>
2023-11-17 05:11:43 +0000

    Render works. More or less

--- module.py
+++ module.py
@@ -9,6 +9,11 @@ class ConflictingDepths(Exception): # Not sure if subclassing Exception is the b
     def __str__(self): return self.message
 """
 
+# inspect that ugly ass error
+# go through to-do list
+# doc strings w/ tags
+
+
 # Okay, so we need a data structure that stores actual mnemonic and a ponter to the data
 
 # We also have axes in tracks. Both of those will have display names + keys by which to identify them.
@@ -33,7 +38,7 @@ class Data():
         self.values = values
         self.mnemonic = mnemonic
     def info(self):
-        return  { "data" : {'mnemonic': self.mnemonic, 'shape': self.values.shape} }
+        return  { "data" : {'mnemonic': self.mnemonic, 'shape': self.values.shape } }
 
 class Graph():
     def __init__(self, *args, **kwargs):
@@ -59,7 +64,7 @@ class Graph():
                 ## Create Y-Axis
                 # Probably need this idea of flattening y axes
                 if self.yaxisname in ar.curves.keys():
-                    self.yaxis.append(ar.curves[self.yaxisname])
+                    self.yaxis.append(ar.curves[self.yaxisname].data)
                 else:
                     self.yaxis.append(ar.index)
                     if not indexOK:
@@ -82,11 +87,65 @@ class Graph():
                     self.track_by_id[id(newTrack)] = newTrack
                     
     
-    def get_data(self):
+    def get_named_tree(self):
         result = []
         for track in self.tracks_ordered:
-            result.append(track.get_data())
-        return {'graph': result}
+            result.append(track.get_named_tree())
+        return { 'graph': result }
+    def get_unnamed_tree(self):
+        result = []
+        for track in self.tracks_ordered:
+            result.append(track.get_unnamed_tree())
+        return result
+    def get_layout(self):
+        num_tracks = len(self.tracks_ordered) 
+        margin = .01 # this will have to be changeable
+        start = .01 # this might change
+        waste_space = start + (num_tracks-1) * .01
+        width = (1 - waste_space) / num_tracks
+
+        args = {}
+        # There will be no number line here! Let's deal with that later!
+        # We also don't know if this work
+        i_axes = 1
+        for track in self.tracks_ordered: # this is what we do for every track    
+            axis_key = "xaxis"
+            for i in range(1, track.count_axes()+1):
+                axis_key += str(i_axes)
+                args[axis_key] = dict(domain = [start, start + width])
+                #print("args["+axis_key+"]  = dict(domain= ["+str(start)+", "+str(start + width)+"])")
+                i_axes += 1
+            start += width + margin
+        layout = go.Layout(**args)
+        return layout
+
+    def get_traces(self):
+        traces = []
+        tree = self.get_unnamed_tree()
+        num_axes = 1
+        for track in tree:
+            for axis in track[0]:
+                for data in axis:
+                    suffix = str(num_axes)
+                    traces.append(go.Scatter(x=data.values,
+                        y=data.index,
+                        xaxis='x' + suffix,
+                        yaxis='y'
+                    ))
+                    #print("go.Scatter(xaxis=x"+str(num_axes) + ")")
+                num_axes += 1
+            for axis in track[1]:
+                for data in axis:
+                    suffix = str(num_axes)
+                    traces.append(go.Scatter(x=data.values,
+                        y=data.index,
+                        xaxis='x' + suffix,
+                        yaxis='y'
+                    ))
+                    #print("go.Scatter(xaxis=x"+str(num_axes) + ")")
+                num_axes += 1
+        return traces
+            
 
 # TODO: how would we accept multiple data
 # What do we do with multiple data?
@@ -117,15 +176,25 @@ class Track():
         # There will have to be a switch for this
         self.axes_below.append(newAxis)
         self.axes_by_id[id(newAxis)] = newAxis # c style lol
+    def count_axes(self):
+        return len(self.axes)
 
-    def get_data(self):
+    def get_named_tree(self):
         above = []
         below = []
         for axis in reversed(self.axes_above):
-            above.append(axis.get_data())
+            above.append(axis.get_named_tree())
         for axis in reversed(self.axes_below):
-            below.append(axis.get_data())
-        return { "track": { self.name: {"above": above, "below": below} } }
+            below.append(axis.get_named_tree())
+        return { "track": { self.name: { "above": above, "below": below } } }
+    def get_unnamed_tree(self):
+        above = []
+        below = []
+        for axis in reversed(self.axes_above):
+            above.append(axis.get_unnamed_tree())
+        for axis in reversed(self.axes_below):
+            below.append(axis.get_unnamed_tree())
+        return [above, below]
 
 # Axis can take multiple data
 # It will place all the data on the same axis
@@ -142,11 +211,16 @@ class Axis():
             self.data = data
         self.name = kwargs.get('name', self.data[0].mnemonic)
         self.display_name = kwargs.get('display_name', self.name)
-    def get_data(self):
+    def get_named_tree(self):
         result = []
         for el in self.data:
             result.append(el.info())
-        return { "axis" : {self.name: result} }
+        return { "axis" : { self.name: result } }
+    def get_unnamed_tree(self):
+        result = []
+        for el in self.data:
+            result.append(el)
+        return result
 
 # Explore ideas of creating each item individually, just take notes
 # Then I guess we have to render (which isn't that different then list, really) )yes, we have to render(
@@ -178,3 +252,5 @@ class Axis():
 
 
 # they all need destructors
+
+# this tree thign is bad, shoudl probably be like "get tracks" "get axis" etc
