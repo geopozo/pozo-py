2265d8a977cad56de0abc51876e04d8fcc9bd919
Andrew Pikul <ajpikul@gmail.com>
2023-12-16 17:48:38 +0000

    Use colour package and refactor theme a bit

--- pozo/themes/__init__.py
+++ pozo/themes/__init__.py
@@ -1,3 +1,5 @@
+import colour
+
 default_color_list = ['#636EFA', '#EF553B', '#00CC96', '#AB63FA', '#FFA15A', '#19D3F3', '#FF6692', '#B6E880', '#FF97FF', '#FECB52']
 default_theme = { "color": default_color_list, }
 
@@ -14,9 +16,8 @@ class Themeable():
 class ColorWheel():
     def __init__(self, color_list=default_color_list.copy()):
         self._color_list = color_list
-        self.restart_counter()
-    def restart_counter(self):
         self._i = 0
+
     def get_color(self):
         color = self._color_list[self._i % len(self._color_list)]
         self._i += 1
@@ -25,16 +26,24 @@ class ColorWheel():
 # We could override the indexer but it's a lot of work and I'm the only one who would see it for now
 class ThemeList(list):
     def __init__(self, *iterable, **kwargs):
-        self._theme_override = kwargs.pop('theme_override', None)
+        self._theme_override = self._process_theme(kwargs.pop('theme_override', {}).copy())
         super().__init__()
         for it in iterable:
             self.add_theme(it)
+
+    def _process_theme(self, theme):
+        if theme is None or (isinstance(theme, dict) and len(theme) == 0): return None
+        if theme is not None and not isinstance(theme, dict):
+            raise TypeError("Can't add theme that is not a dict or None")
+        if theme is not None and "color" in theme and isinstance(theme["color"], list):
+            theme["color"] = ColorWheel(theme["color"])
+        return theme
+
     def add_theme(self, theme):
         if theme is not None:
-            theme = theme.copy()
-            if theme and "color" in theme and isinstance(theme["color"], list):
-                theme["color"] = ColorWheel(theme["color"])
+            theme = self._process_theme(theme.copy())
         super().append(theme)
+
     def get_key(self, key):
         if self._theme_override is not None and key in self._theme_override:
             return self._theme_override[key]
@@ -48,8 +57,9 @@ class ThemeList(list):
         if color is None:
             raise ValueError("Couldn't find color in theme list. A fallback is hardcoded into the package, how did this happen?")
         elif isinstance(color, str):
-            return color
+            return colour.Color(color).hex_l
         elif isinstance(color, ColorWheel):
-            return color.get_color()
-        # also check for color object (python)
+            return colour.Color(color.get_color()).hex_l
+        elif isinstance(color, colour.Color):
+            return color.hex_l
 
