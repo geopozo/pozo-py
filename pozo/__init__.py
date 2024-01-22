137ef1a55567ad05cfbac5f4cd781d15ae4f0a45
Andrew Pikul <ajpikul@gmail.com>
2024-01-22 03:41:39 +0000

    Add HasLog selector

--- pozo/__init__.py
+++ pozo/__init__.py
@@ -6,8 +6,28 @@ from .graphs import Graph
 import pozo.themes as themes
 import pozo.renderers as renderers
 import pozo.units as units
+import ood
 
 def deLASio(mnemonic):
     return mnemonic.split(":", 1)[0] if ":" in mnemonic else mnemonic
 
 # Error on dimensionless
+#
+
+
+class HasLog(ood.selectors.Selector):
+    def __init__(self, mnemonic, prefix=''):
+        self.mnemonic = mnemonic
+        self.prefix = prefix
+    def _process(self, parent):
+        ret_items = []
+        for item in parent.get_items():
+            if hasattr(item, 'get_mnemonic'):
+                if item.get_mnemonic() == self.mnemonic:
+                    ret_items.append(item)
+                    break
+            if isinstance(item, ood.Observer):
+                if item.has_item(HasLog(self.mnemonic, prefix='-' + self.prefix)):
+                    ret_items.append(item)
+                    break
+        return ret_items
