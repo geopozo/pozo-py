5632709a38b4362a4823f000ad8675f9ccade8c7
Neyberson <neyberatencio@gmail.com>
2024-03-26 16:22:49 -0500

    Add .original_data to Graph constructor

--- pozo/graphs.py
+++ pozo/graphs.py
@@ -18,6 +18,7 @@ class Graph(ood.Observer, pzt.Themeable):
     _child_type="track"
 
     def __init__(self, *args, **kwargs):
+        self.original_data = None
         self._render = {}
         render_keys = ['show_depth', 'depth_position', 'height', 'depth']
         for key in render_keys:
