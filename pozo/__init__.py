3eced3a5dd1d1bd5d5ffe722fc26875f8c0c335e
Andrew Pikul <ajpikul@gmail.com>
2024-05-18 19:10:59 +0000

    Add better comments

--- pozo/__init__.py
+++ pozo/__init__.py
@@ -1,5 +1,6 @@
 import gettext
 from pathlib import Path
+
 import ood
 import pint
 import numpy as np
@@ -33,28 +34,33 @@ locale_dir = Path(__file__).resolve().parents[0] / "locale"
 es_translations = gettext.translation('pozo', localedir=locale_dir, languages=['es'])
 en_translations = gettext.translation('pozo', localedir=locale_dir, languages=['en'])
 
+# es() is a shortcut to set the language to spanish
 def es(t = es_translations):
     global current_translator
     current_translator = t.gettext
+
+# en is a shortcut to set the language to english
 def en(t = en_translations):
     global current_translator
     current_translator = t.gettext
 
+# doc() decorate allows us to use @doc(_("documentation")) so that we can use gettext with pydoc/docstrings
 def doc(docstring):
     def decorate(obj):
         obj.__doc__ = docstring
         return obj
     return decorate
 
+# PozoWarning is jsut a UserWarning but we can detect if we raise it with isinstance
 class PozoWarning(UserWarning):
     pass
 
-# These are all utility functions
+# deLASio extracts the actual mnemonic from the kinverity1/lasio suffixed mnemonic
 def deLASio(mnemonic):
     return mnemonic.split(":", 1)[0] if ":" in mnemonic else mnemonic
 
-# is_array use the input data to verify if is pint data or other type that has
-# __len__ and return a boolean. Be careful with this, it will return true for Pozo objects.
+# is_array checks to see that the input is some type of array, even if wrapped by pint.Quantity.
+# becareful, it can return false positives if they have a __len__ attribute
 def is_array(value):
     if isinstance(value, str): return False
     if isinstance(value, pint.Quantity):
@@ -71,6 +77,8 @@ def is_scalar_number(value):
         return is_scalar_number(value.magnitude)
     return isinstance(value, number_types)
 
+# HasLog is a selector which will traverse through all children every generation of an object
+# and look for those that have a certain mnemonic
 class HasLog(ood.selectors.Selector):
     def __init__(self, mnemonic):
         self.mnemonic = mnemonic
@@ -87,16 +95,18 @@ class HasLog(ood.selectors.Selector):
     def __repr__(self):
         return f"HasLog({self.mnemonic})"
 
-# verify_array_len use three inputs to verify the lenght in the data
+# verify_array_len is a shortcut to make sure all input data is the same shape
 def verify_array_len(constant, data):
     if is_array(constant) and len(constant) != len(data): return False
     return True
 
+# str_to_HasLog will pre-process selectors: a string selector in ood normally causes a name lookup,
+# but this transforms it to a HasLog selector object. If you wanted a name, you could do {'name':'whatever'}
 def str_to_HasLog(argument):
     if isinstance(argument, (list, tuple)):
         ret = []
         for selector in argument:
-                ret.append(str_to_HasLog(selector))
+                ret.append(str_to_HasLog(selector)) # can't support nested lists, that's okay
         return ret
     elif isinstance(argument, str):
         return HasLog(argument)
