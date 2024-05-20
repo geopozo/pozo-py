5888dacadcc9ffa589e090c60d707e48f4d8f449
Neyberson <neyberatencio@gmail.com>
2024-05-20 12:27:14 -0500

    Format code at utils test

--- tests/utils/test_init.py
+++ tests/utils/test_init.py
@@ -4,16 +4,17 @@ import pandas as pd
 import polars as pl
 import pozo.utils as pzutils
 
-list_data = [float('inf'), -float('inf'), None]
+list_data = [float("inf"), -float("inf"), None]
 list_data_irregular = [0, 20, 40, 50, 70, 90, 300, 301, 330, 400]
 np_data = np.linspace(0, 2000, num=50, endpoint=True)
 np_data_irregular = np.array(list_data_irregular)
 series = pd.Series(np_data)
 series_data_irregular = pd.Series(np_data_irregular)
-df_pandas = pd.DataFrame({"depth":list_data_irregular})
+df_pandas = pd.DataFrame({"depth": list_data_irregular})
 series_polars = pl.Series(np_data)
 series_polars_data_irregular = pl.Series(np_data_irregular)
-df_polars = pl.DataFrame({"depth":list_data_irregular})
+df_polars = pl.DataFrame({"depth": list_data_irregular})
+
 
 def test_summarize_array():
     assert pzutils.summarize_array(np_data) is not None
@@ -25,15 +26,73 @@ def test_summarize_array():
     assert pzutils.summarize_array(series_polars_data_irregular) is not None
     assert pzutils.summarize_array(list_data_irregular) is None
 
+
 def test_is_close():
-    assert pzutils.is_close(np_data[0], np_data[-1], True, np_data[1]-np_data[0], 0.001) is not None
-    assert pzutils.is_close(series[0], series[-1], True, series[1]-series[0], 0.001) is not None
-    assert pzutils.is_close(series_polars[0], series_polars[-1], True, series_polars[1]-series_polars[0], 0.001) is not None
-    assert pzutils.is_close(list_data[0], list_data[-1], True, list_data[1]-list_data[0], 0.001) is not None
-    assert pzutils.is_close(np_data_irregular[0], np_data_irregular[-1], True, np_data_irregular[1]-np_data_irregular[0], 0.001) is not None
-    assert pzutils.is_close(series_data_irregular[0], series_data_irregular[-1], True, series_data_irregular[1]-series_data_irregular[0], 0.001) is not None
-    assert pzutils.is_close(series_polars_data_irregular[0], series_polars_data_irregular[-1], True, series_polars_data_irregular[1]-series_polars_data_irregular[0], 0.001) is not None
-    assert pzutils.is_close(list_data_irregular[0], list_data_irregular[-1], True, list_data_irregular[1]-list_data_irregular[0], 0.001) is not None
+    assert (
+        pzutils.is_close(np_data[0], np_data[-1], True, np_data[1] - np_data[0], 0.001)
+        is not None
+    )
+    assert (
+        pzutils.is_close(series[0], series[-1], True, series[1] - series[0], 0.001)
+        is not None
+    )
+    assert (
+        pzutils.is_close(
+            series_polars[0],
+            series_polars[-1],
+            True,
+            series_polars[1] - series_polars[0],
+            0.001,
+        )
+        is not None
+    )
+    assert (
+        pzutils.is_close(
+            list_data[0], list_data[-1], True, list_data[1] - list_data[0], 0.001
+        )
+        is not None
+    )
+    assert (
+        pzutils.is_close(
+            np_data_irregular[0],
+            np_data_irregular[-1],
+            True,
+            np_data_irregular[1] - np_data_irregular[0],
+            0.001,
+        )
+        is not None
+    )
+    assert (
+        pzutils.is_close(
+            series_data_irregular[0],
+            series_data_irregular[-1],
+            True,
+            series_data_irregular[1] - series_data_irregular[0],
+            0.001,
+        )
+        is not None
+    )
+    assert (
+        pzutils.is_close(
+            series_polars_data_irregular[0],
+            series_polars_data_irregular[-1],
+            True,
+            series_polars_data_irregular[1] - series_polars_data_irregular[0],
+            0.001,
+        )
+        is not None
+    )
+    assert (
+        pzutils.is_close(
+            list_data_irregular[0],
+            list_data_irregular[-1],
+            True,
+            list_data_irregular[1] - list_data_irregular[0],
+            0.001,
+        )
+        is not None
+    )
+
 
 def test_hash_array():
     assert pzutils.hash_array(np_data) is not None
