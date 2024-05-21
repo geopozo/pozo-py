b425f7869abc8665be2747d58545b001c693107f
Neyberson <neyberatencio@gmail.com>
2024-05-21 15:03:16 -0500

    Import some functions from array module to __init__ at utils folder

--- pozo/utils/__init__.py
+++ pozo/utils/__init__.py
@@ -1,3 +1,20 @@
+from .array import (
+    is_array,
+    is_close,
+    is_scalar_number,
+    hash_array,
+    isfinite,
+    isnan,
+    linspace,
+    append,
+    count_nonzero,
+    max,
+    min,
+    nanquantile,
+    summarize_array,
+)
+
+
 # These are all utility functions
 # Taken by the principal __ini__.py
 def deLASio(mnemonic):
