0fd798647e733c2e72ee0028f39c8a3493298e00
Neyberson <neyberatencio@gmail.com>
2024-05-30 09:25:29 -0500

    Create new variables at test_array.py

--- tests/utils/test_array.py
+++ tests/utils/test_array.py
@@ -10,19 +10,23 @@ np_data = np.linspace(0, 2000, num=50, endpoint=True)
 np_data_irregular = np.array(list_data_irregular)
 series = pd.Series(np_data)
 series_data_irregular = pd.Series(np_data_irregular)
+df_pandas = pd.DataFrame({"depth": np_data})
 df_pandas_irregular = pd.DataFrame({"depth": list_data_irregular})
 series_polars = pl.Series(np_data)
 series_polars_data_irregular = pl.Series(np_data_irregular)
+df_polars = pl.DataFrame({"depth": np_data})
 df_polars_irregular = pl.DataFrame({"depth": list_data_irregular})
 
 
 def test_summarize_array():
     assert pzutils.summarize_array(np_data) is not None
+    assert pzutils.summarize_array(df_polars["depth"]) is not None
     assert pzutils.summarize_array(df_polars_irregular["depth"]) is not None
     assert pzutils.summarize_array(series) is not None
     assert pzutils.summarize_array(series_polars) is not None
     # assert pzutils.summarize_array(list_data) is None #This function doesn't use elements like inf or NaN
     assert pzutils.summarize_array(np_data_irregular) is not None
+    assert pzutils.summarize_array(df_pandas["depth"]) is not None
     assert pzutils.summarize_array(df_pandas_irregular["depth"]) is not None
     assert pzutils.summarize_array(series_data_irregular) is not None
     assert pzutils.summarize_array(series_polars_data_irregular) is not None
