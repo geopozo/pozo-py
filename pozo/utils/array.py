e75628b83946b300d2d88f12c8077e8297da66a6
Neyberson <neyberatencio@gmail.com>
2024-05-30 10:48:53 -0500

    Improve some validations at summarize_array with try except at array module

--- pozo/utils/array.py
+++ pozo/utils/array.py
@@ -15,10 +15,16 @@ from .docs import doc
 def summarize_array(depth):
     if not is_array(depth):
         raise ValueError(_("You must use for depth a list, tuple or an numpy array"))
-    if False in isfinite(depth):
-        raise ValueError(
-            _("You mustn't use float('inf'), -float('inf') and/or float('nan')")
-        )
+    try:
+        if False in isfinite(depth):
+            raise ValueError(
+                _("You mustn't use float('inf'), -float('inf') and/or float('nan')")
+            )
+    except TypeError:
+        if isfinite(depth).isin([False]).any():
+            raise ValueError(
+                _("You mustn't use float('inf'), -float('inf') and/or float('nan')")
+            )
 
     starts = []
     stops = []
