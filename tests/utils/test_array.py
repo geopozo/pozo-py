95e241634a2e885c00e199fadd5a8fac5a8b3c1e
Neyberson <neyberatencio@gmail.com>
2024-05-30 14:53:35 -0500

    Fixbug at asserts from test_summarize_array function

--- tests/utils/test_array.py
+++ tests/utils/test_array.py
@@ -21,13 +21,13 @@ df_polars_irregular = pl.DataFrame({"depth": list_data_irregular})
 def test_summarize_array():
     assert pzutils.summarize_array(np_data) is not None
     assert pzutils.summarize_array(df_polars["depth"]) is not None
-    assert pzutils.summarize_array(df_polars_irregular["depth"]) is None
+    assert pzutils.summarize_array(df_polars_irregular["depth"]) is not None
     assert pzutils.summarize_array(series) is not None
     assert pzutils.summarize_array(series_polars) is not None
     # assert pzutils.summarize_array(list_data) is None #This function doesn't use elements like inf or NaN
     assert pzutils.summarize_array(np_data_irregular) is not None
     assert pzutils.summarize_array(df_pandas["depth"]) is not None
-    assert pzutils.summarize_array(df_pandas_irregular["depth"]) is None
+    assert pzutils.summarize_array(df_pandas_irregular["depth"]) is not None
     assert pzutils.summarize_array(series_data_irregular) is not None
     assert pzutils.summarize_array(series_polars_data_irregular) is not None
     assert pzutils.summarize_array(list_data_irregular) is not None
