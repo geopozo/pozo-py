e8e752b6d1d438ba67230c7116fbc2e06ca77f82
David Angarita <davidangarita.sistemas@gmail.com>
2025-05-27 11:44:35 -0500

    Refactor conditionals

--- pozo/units/units.py
+++ pozo/units/units.py
@@ -6,24 +6,33 @@ import pint  # noqa
 
 import pozo
 
-os.environ['PINT_ARRAY_PROTOCOL_FALLBACK'] = "0" # from numpy/pint documentation
+os.environ["PINT_ARRAY_PROTOCOL_FALLBACK"] = "0"  # from numpy/pint documentation
+
 
 class MissingLasUnitWarning(UserWarning):
     """Warning for unresolved LAS units."""
+
     pass
+
 
 class UnitException(Exception):
     """Raised when unit parsing fails."""
+
     pass
+
 
 class MissingRangeError(Exception):
     """Raised when no matching range is found for LAS data."""
+
     pass
 
-class LasMapEntry():
-    def  __init__(self, boundaries, unit, confidence):
+
+class LasMapEntry:
+    def __init__(self, boundaries, unit, confidence):
         if not isinstance(boundaries, tuple) or len(boundaries) not in {0, 2}:
-            raise TypeError("add_las_map ranges should contain a tuple with (min, max) or () catch-all")
+            raise TypeError(
+                "add_las_map ranges should contain a tuple with (min, max) or () catch-all"
+            )
 
         self.boundaries = boundaries
         self.unit = unit
@@ -36,6 +45,7 @@ class LasMapEntry():
             return True
         return False
 
+
 # Namespace would be nicer and I could hide registries if this wasn't overriden
 # But would the map be global?
 # Or would we just use a default register like in init
@@ -47,31 +57,44 @@ class LasRegistry(pint.UnitRegistry):
 
     def add_las_map(self, mnemonic, unit, ranges, confidence="- not indicated - LOW"):
         if not isinstance(ranges, list):
-            if not isinstance(ranges, LasMapEntry):
-                ranges = [LasMapEntry((), ranges, confidence)]
-            else:
-                ranges = [ranges]
+            ranges = (
+                [LasMapEntry((), ranges, confidence)]
+                if not isinstance(ranges, LasMapEntry)
+                else [ranges]
+            )
+
         for ra in ranges:
             if not isinstance(ra, LasMapEntry):
-                raise TypeError("Improperly formated map for LasMap, should be type LasMapEntry")
-            self.parse_units(ra.unit) # Can't check in LasMapEntry because don't have registry
+                raise TypeError(
+                    "Improperly formated map for LasMap, should be type LasMapEntry"
+                )
+            # Can't check in LasMapEntry because don't have registry
+            self.parse_units(ra.unit)
+
         if mnemonic not in self._mnemonic_units:
             self._mnemonic_units[mnemonic] = {}
             self._reverse_mnemonic_units[mnemonic] = {}
+
         self._mnemonic_units[mnemonic][unit] = ranges
+
         for ra in ranges:
-            self._reverse_mnemonic_units[mnemonic][self.parse_units(ra.unit)] = unit # doesn't this override
+            self._reverse_mnemonic_units[mnemonic][self.parse_units(ra.unit)] = (
+                unit  # doesn't this override
+            )
 
     def resolve_SI_unit_to_las(self, mnemonic, unit):
         unit = unit if isinstance(unit, pint.Unit) else self.parse_units(unit)
         # TODO: looking up the dimension would be nice
         mnemonic = pozo.deLASio(mnemonic)
-        if mnemonic in self._reverse_mnemonic_units and unit in self._reverse_mnemonic_units[mnemonic]:
+        if (
+            mnemonic in self._reverse_mnemonic_units
+            and unit in self._reverse_mnemonic_units[mnemonic]
+        ):
             return self._reverse_mnemonic_units[mnemonic][unit]
         elif unit in self._reverse_mnemonic_units["-"]:
             return self._reverse_mnemonic_units["-"][unit]
-        else: return None
-
+        else:
+            return None
 
     def resolve_las_unit(self, mnemonic, unit, data):
         mnemonic = pozo.deLASio(mnemonic)
@@ -87,10 +110,13 @@ class LasRegistry(pint.UnitRegistry):
             for ra in ranges:
                 if ra.check(min_val, max_val):
                     return ra
-            raise MissingRangeError(f"{unit} for {mnemonic} found but not in range: {ranges}.")
+            raise MissingRangeError(
+                f"{unit} for {mnemonic} found but not in range: {ranges}."
+            )
         return None
 
-    def parse_unit_from_context(self, mnemonic, unit, data): # this just gives you a result
+    # this just gives you a result
+    def parse_unit_from_context(self, mnemonic, unit, data):
         resolved = None
         try:
             resolved = self.resolve_las_unit(mnemonic, unit, data)
@@ -104,14 +130,17 @@ class LasRegistry(pint.UnitRegistry):
                 return None
         else:
             try:
-                if not unit or unit == "": raise UnitException("Empty unit not allowed- please map it")
+                if not unit or unit == "":
+                    raise UnitException("Empty unit not allowed- please map it")
                 try:
                     return self.parse_units(unit)
                 except Exception as e:
                     warnings.warn(f"Couldn't parse unit: {e}", MissingLasUnitWarning)
                     return None
             except pint.UndefinedUnitError as e:
-                raise UnitException(f"'{unit}' for '{pozo.deLASio(mnemonic)}' not found.") from e
+                raise UnitException(
+                    f"'{unit}' for '{pozo.deLASio(mnemonic)}' not found."
+                ) from e
+
 
 # we now don't get confidence from string
-
