e0e2545bca4e2c9b3fe04991720d0a71758a083e
David Angarita <davidangarita.sistemas@gmail.com>
2025-05-29 10:09:45 -0500

    Separe new registry mapping

--- pozo/units/__init__.py
+++ pozo/units/__init__.py
@@ -5,12 +5,13 @@ from IPython.display import HTML, display
 from io import StringIO
 import pandas as pd
 
+from .registry_config import registry_defines, registry_mapping
+
 from .units import (
     MissingRangeError,
     UnitException,
     MissingLasUnitWarning,
     LasRegistry,
-    LasMapEntry,
 )
 import re
 
@@ -19,55 +20,9 @@ Quantity = Q = (
     registry.Quantity
 )  # Overriding the registry and all this is a little weird
 
-registry.define('gamma_API_unit = [Gamma_Ray_Tool_Response]  = gAPI')
-registry.define("porosity_unit = percent = pu")
-registry.define("of_1 = 100 * percent = fraction")
-registry.define("legacy_api_porosity_unit = [Legacy_API_Porosity_Unit] = puAPI")
+registry_defines(registry)
+registry_mapping(registry)
 
-PU = [
-        LasMapEntry((-50, 80), "pu", "decide by range - MEDIUM"),
-        LasMapEntry((-.5, .8), "of_1", "decide by range - MEDIUM"),
-        LasMapEntry((), "puAPI", "catch all, legacy unit - LOW"),
-    ]
-percent_general = [
-        LasMapEntry((-100, 100), "percent", "decide by range, verify - LOW"),
-        LasMapEntry((-1, 1), "of_1", "decide by range, verify - LOW"),
-        LasMapEntry((), "ppm", "catch all, ppm - LOW" ),
-    ]
-
-registry.add_las_map('-',    'MM'  , 'millimeter'              , "decided without mnemonic- MEDIUM")
-registry.add_las_map('-',    'M'   , 'meter'                   , "decided without mnemonic- MEDIUM")
-registry.add_las_map('-',    'CM'  , 'centimeter'              , "decided without mnemonic- MEDIUM")
-registry.add_las_map('-',    'GAPI', 'gAPI'                    , "decided without mnemonic- MEDIUM")
-registry.add_las_map('-',    'DEG_C', 'celsius'                    , "decided without mnemonic- MEDIUM")
-registry.add_las_map('-',    'DEG_F', 'fahrenheit'                    , "decided without mnemonic- MEDIUM")
-registry.add_las_map('-',    'COUNTS/S', 'cps'                    , "decided without mnemonic- MEDIUM")
-registry.add_las_map('-',    'CPS', 'cps'                    , "decided without mnemonic- MEDIUM")
-registry.add_las_map('DEPT', 'M'   , 'meter'                   , "HIGH")
-registry.add_las_map('DEPT', 'FT'  , 'feet'                    , "HIGH")
-registry.add_las_map('DEPT', 'F'  ,  'feet'                    , "HIGH")
-registry.add_las_map('GR'  , 'GAPI', 'gAPI'                    , "HIGH")
-registry.add_las_map('GR'  , '',     'gAPI'                    , "HIGH")
-registry.add_las_map('CGR' , 'GAPI', 'gAPI'                    , "HIGH")
-registry.add_las_map('CALI', 'CM'  , 'centimeter'              , "HIGH")
-registry.add_las_map('CALI', 'IN'  , 'inch'                    , "HIGH")
-registry.add_las_map('DRHO', 'G/C3', 'gram / centimeter ** 3'  , "HIGH")
-registry.add_las_map('RHOB', 'G/C3', 'gram / centimeter ** 3'  , "HIGH")
-registry.add_las_map('DT'  , 'US/F', 'microsecond / foot'      , "HIGH")
-registry.add_las_map('ILD' , 'OHMM', 'ohm * meter'             , "HIGH")
-registry.add_las_map('LLS' , 'OHMM', 'ohm * meter'             , "HIGH")
-registry.add_las_map('ILM' , 'OHMM', 'ohm * meter'             , "HIGH")
-registry.add_las_map('LLD' , 'OHMM', 'ohm * meter'             , "HIGH")
-registry.add_las_map('SFL' , 'OHMM', 'ohm * meter'             , "HIGH")
-registry.add_las_map('SGR' , 'GAPI', 'gAPI'                    , "HIGH")
-registry.add_las_map('SP'  , 'MV'  , 'millivolt'               , "HIGH")
-registry.add_las_map('MSFL', 'OHMM', 'ohm * meter'             , "HIGH")
-registry.add_las_map('NPHI', 'PU'  , PU)
-registry.add_las_map('NPHI', 'PU%' , 'pu'                      , "HIGH")
-registry.add_las_map('PEF' , ''    , percent_general)
-registry.add_las_map('POTA', ''    , percent_general)
-registry.add_las_map('THOR', ''    , percent_general)
-registry.add_las_map('URAN', ''    , percent_general)
 
 # TODO: Beginning of a database, has to go online
 # mnemonics, synonyms (resolve- matching in theme,
