6347c43ff86d41bd21cc689e68e739f72a6112a9
David Angarita <davidangarita.sistemas@gmail.com>
2025-06-06 08:39:28 -0500

    Change comment

--- pozo/renderers/plotly.py
+++ pozo/renderers/plotly.py
@@ -639,6 +639,7 @@ class Plotly(pzr.Renderer):
                                     fillcolor)
                                 # we will lock both axes completely but it seems like mutual zoom operations are ok
                                 # notes above on how to do that
+                                # We do not support 'intervals'
                         if False:
                             pass
                         else:
