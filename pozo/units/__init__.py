327f0256fa3a41b92f393d52b6ff0c1d32deb212
David Angarita <davidangarita.sistemas@gmail.com>
2025-06-25 19:14:13 -0500

    Add initial functions for units

--- pozo/units/__init__.py
+++ pozo/units/__init__.py
@@ -9,3 +9,15 @@ os.environ["PINT_ARRAY_PROTOCOL_FALLBACK"] = "0"  # from numpy/pint documentatio
 las_registry = LasUnitRegistry()
 unit_registry = pint.UnitRegistry()
 Quantity = Q = unit_registry.Quantity
+
+
+def check_las(data):
+    pass
+
+
+def parse_unit_from_curve(curve):
+    pass
+
+
+def set_unit_registry(new_registry):
+    pass
