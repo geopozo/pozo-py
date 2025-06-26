516e5da4a057ff537d59ab2b9ef3c02fae5edcd9
David Angarita <davidangarita.sistemas@gmail.com>
2025-06-26 18:38:14 -0500

    Move RangeBondaries to new file

---
+++ pozo/units/range_bondaries.py
@@ -0,0 +1,17 @@
+class RangeBoundaries:
+    def __init__(self, boundaries, unit, confidence):
+        if not isinstance(boundaries, tuple) or len(boundaries) not in {0, 2}:
+            raise TypeError(
+                "boundaries should contain a tuple with (min, max) or () catch-all"
+            )
+
+        self.boundaries = boundaries
+        self.unit = unit
+        self.confidence = confidence
+
+    def is_within_range(self, min_val, max_val):
+        if len(self.boundaries) == 0:
+            return True
+        elif min_val > self.boundaries[0] and max_val < self.boundaries[1]:
+            return True
+        return False
