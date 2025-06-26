4d67a79894594678551b2b00520b06187fffd96e
David Angarita <davidangarita.sistemas@gmail.com>
2025-06-26 18:15:11 -0500

    Add parse_unit_safe and parse_unit_from_curve

--- pozo/traces.py
+++ pozo/traces.py
@@ -136,7 +136,7 @@ class Trace(Drawable):
         self, unit
     ):  # Call this early, in set_data, in set_depth, in init()
         if isinstance(unit, str):
-            return pzu.parse(unit)
+            return pzu.parse_unit_safe(unit)
         elif not isinstance(unit, pint.Unit) and unit is not None:
             raise pint.UndefinedUnitError(str(type(unit))) from TypeError(
                 f"Unrecognized unit type: {unit}"
@@ -145,7 +145,7 @@ class Trace(Drawable):
 
     def set_unit(self, unit):
         if isinstance(unit, str):
-            unit = pzu.parse(unit)
+            unit = pzu.parse_unit_safe(unit)
         elif isinstance(unit, pint.Unit) or unit is None:
             pass
         else:
@@ -159,7 +159,7 @@ class Trace(Drawable):
 
     def set_depth_unit(self, unit):
         if isinstance(unit, str):
-            unit = pzu.parse(unit)
+            unit = pzu.parse_unit_safe(unit)
         elif isinstance(unit, pint.Unit) or unit is None:
             pass
         else:
--- pozo/units/__init__.py
+++ pozo/units/__init__.py
@@ -1,7 +1,12 @@
 import os
+import warnings
 
-from pint import UnitRegistry, get_application_registry, set_application_registry
+from lasio import CurveItem
+from pint import UnitRegistry, get_application_registry, Unit
 
+import pozo
+
+from .errors import MissingLasUnitWarning, MissingRangeError, UnitException
 from .registry_config import registry_defines, registry_mapping
 from .units import LasMap
 
@@ -19,9 +24,32 @@ def check_las(data):
     pass
 
 
-def parse_unit_from_curve(curve):
-    pass
+def parse_unit_safe(unit: str):
+    try:
+        return registry.parse_units(unit)
+    except Exception as e:
+        warnings.warn(f"Couldn't parse unit: {e}", MissingLasUnitWarning)
+        return None
 
 
-def set_unit_registry(registry: UnitRegistry):
-    set_application_registry(registry)
+def _try_parse_unit_with_fallback(unit: str, mnemonic: str) -> Unit:
+    try:
+        return registry.parse_units(unit)
+    except Exception as e:
+        raise UnitException(
+            f"'{unit}' for '{pozo.deLASio(mnemonic)}' not found."
+        ) from e
+
+
+def parse_unit_from_curve(curve: CurveItem) -> Unit:
+    try:
+        resolved = las_map.resolve_las_unit(curve.mnemonic, curve.unit, curve.data)
+        if resolved is not None:
+            return registry.parse_units(resolved.unit)
+    except MissingRangeError as e:
+        warnings.warn(str(e))
+
+    if not curve.unit or curve.unit == "":
+        raise UnitException("Empty unit not allowed- please map it")
+
+    return _try_parse_unit_with_fallback(curve.unit, curve.mnemonic)
