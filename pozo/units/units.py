420dfe8375210f09c4f2694544ab0a764bd1593b
David Angarita <davidangarita.sistemas@gmail.com>
2025-06-26 18:39:00 -0500

    Create add_las_map function in LasMap

--- pozo/units/units.py
+++ pozo/units/units.py
@@ -1,7 +1,30 @@
+from .range_bondaries import RangeBoundaries
+
+
 class LasMap:
     def __init__(self):
-        self._mnemonic_to_units = {}
-        self._units_to_mnemonic = {}
+        self.mnemonic_to_units = {}
+        self.units_to_mnemonic = {}
 
-    def add_las_map(self):
-        pass
+    def add_las_map(self, mnemonic, unit, ranges, confidence="- not indicated - LOW"):
+        if not isinstance(ranges, tuple):
+            ranges = (
+                [RangeBoundaries((), ranges, confidence)]
+                if not isinstance(ranges, RangeBoundaries)
+                else [ranges]
+            )
+
+        for ra in ranges:
+            if not isinstance(ra, RangeBoundaries):
+                raise TypeError("All entries must be of type RangeBoundaries.")
+
+        if mnemonic not in self.mnemonic_to_units:
+            self.mnemonic_to_units[mnemonic] = {}
+            self.units_to_mnemonic[mnemonic] = {}
+
+        self.mnemonic_to_units[mnemonic][unit] = ranges
+
+        for ra in ranges:
+            parsed_unit = ra.unit
+            self.units_to_mnemonic[mnemonic][parsed_unit] = unit
+
