f301d49452deb36c6ea7a0f238c38d6788839a43
David Angarita <davidangarita.sistemas@gmail.com>
2025-06-25 19:12:08 -0500

    Add unit and las registry

---
+++ pozo/units/__init__.py
@@ -0,0 +1,11 @@
+import os
+
+import pint
+
+from .units import LasUnitRegistry
+
+os.environ["PINT_ARRAY_PROTOCOL_FALLBACK"] = "0"  # from numpy/pint documentation
+
+las_registry = LasUnitRegistry()
+unit_registry = pint.UnitRegistry()
+
