5f469265e677e94e1686f08dc9c1545870c2626e
David Angarita <davidangarita.sistemas@gmail.com>
2025-06-26 19:15:26 -0500

    Change data structure

--- pozo/units/registry_config.py
+++ pozo/units/registry_config.py
@@ -1,56 +1,67 @@
-from .units import LasUnitRegistry, RangeBoundaries
+from pint import UnitRegistry
 
+from .units import LasMap, RangeBoundaries
 
-PU = [
+PU = (
     RangeBoundaries((-50, 80), "pu", "decide by range - MEDIUM"),
     RangeBoundaries((-0.5, 0.8), "of_1", "decide by range - MEDIUM"),
     RangeBoundaries((), "puAPI", "catch all, legacy unit - LOW"),
-]
-percent_general = [
+)
+percent_general = (
     RangeBoundaries((-100, 100), "percent", "decide by range, verify - LOW"),
     RangeBoundaries((-1, 1), "of_1", "decide by range, verify - LOW"),
     RangeBoundaries((), "ppm", "catch all, ppm - LOW"),
-]
+)
+
+new_definitions = (
+    "gamma_API_unit = [Gamma_Ray_Tool_Response]  = gAPI",
+    "porosity_unit = percent = pu",
+    "of_1 = 100 * percent = fraction",
+    "legacy_api_porosity_unit = [Legacy_API_Porosity_Unit] = puAPI",
+)
+
+units = (
+    ("-", "MM", "millimeter", "decided without mnemonic- MEDIUM"),
+    ("-", "M", "meter", "decided without mnemonic- MEDIUM"),
+    ("-", "CM", "centimeter", "decided without mnemonic- MEDIUM"),
+    ("-", "GAPI", "gAPI", "decided without mnemonic- MEDIUM"),
+    ("-", "DEG_C", "celsius", "decided without mnemonic- MEDIUM"),
+    ("-", "DEG_F", "fahrenheit", "decided without mnemonic- MEDIUM"),
+    ("-", "COUNTS/S", "cps", "decided without mnemonic- MEDIUM"),
+    ("-", "CPS", "cps", "decided without mnemonic- MEDIUM"),
+    ("DEPT", "M", "meter", "HIGH"),
+    ("DEPT", "FT", "feet", "HIGH"),
+    ("DEPT", "F", "feet", "HIGH"),
+    ("GR", "GAPI", "gAPI", "HIGH"),
+    ("GR", "", "gAPI", "HIGH"),
+    ("CGR", "GAPI", "gAPI", "HIGH"),
+    ("CALI", "CM", "centimeter", "HIGH"),
+    ("CALI", "IN", "inch", "HIGH"),
+    ("DRHO", "G/C3", "gram / centimeter ** 3", "HIGH"),
+    ("RHOB", "G/C3", "gram / centimeter ** 3", "HIGH"),
+    ("DT", "US/F", "microsecond / foot", "HIGH"),
+    ("ILD", "OHMM", "ohm * meter", "HIGH"),
+    ("LLS", "OHMM", "ohm * meter", "HIGH"),
+    ("ILM", "OHMM", "ohm * meter", "HIGH"),
+    ("LLD", "OHMM", "ohm * meter", "HIGH"),
+    ("SFL", "OHMM", "ohm * meter", "HIGH"),
+    ("SGR", "GAPI", "gAPI", "HIGH"),
+    ("SP", "MV", "millivolt", "HIGH"),
+    ("MSFL", "OHMM", "ohm * meter", "HIGH"),
+    ("NPHI", "PU", PU),
+    ("NPHI", "PU%", "pu", "HIGH"),
+    ("PEF", "", percent_general),
+    ("POTA", "", percent_general),
+    ("THOR", "", percent_general),
+    ("URAN", "", percent_general),
+)
 
 
-def registry_defines(registry: LasUnitRegistry):
-    registry.unit_registry.define("gamma_API_unit = [Gamma_Ray_Tool_Response]  = gAPI")
-    registry.unit_registry.define("porosity_unit = percent = pu")
-    registry.unit_registry.define("of_1 = 100 * percent = fraction")
-    registry.unit_registry.define("legacy_api_porosity_unit = [Legacy_API_Porosity_Unit] = puAPI")
+def registry_defines(registry: UnitRegistry) -> None:
+    for definition in new_definitions:
+        registry.define(definition)
 
 
-def registry_mapping(registry: LasUnitRegistry):
-    registry.add_las_map("-", "MM", "millimeter", "decided without mnemonic- MEDIUM")
-    registry.add_las_map("-", "M", "meter", "decided without mnemonic- MEDIUM")
-    registry.add_las_map("-", "CM", "centimeter", "decided without mnemonic- MEDIUM")
-    registry.add_las_map("-", "GAPI", "gAPI", "decided without mnemonic- MEDIUM")
-    registry.add_las_map("-", "DEG_C", "celsius", "decided without mnemonic- MEDIUM")
-    registry.add_las_map("-", "DEG_F", "fahrenheit", "decided without mnemonic- MEDIUM")
-    registry.add_las_map("-", "COUNTS/S", "cps", "decided without mnemonic- MEDIUM")
-    registry.add_las_map("-", "CPS", "cps", "decided without mnemonic- MEDIUM")
-    registry.add_las_map("DEPT", "M", "meter", "HIGH")
-    registry.add_las_map("DEPT", "FT", "feet", "HIGH")
-    registry.add_las_map("DEPT", "F", "feet", "HIGH")
-    registry.add_las_map("GR", "GAPI", "gAPI", "HIGH")
-    registry.add_las_map("GR", "", "gAPI", "HIGH")
-    registry.add_las_map("CGR", "GAPI", "gAPI", "HIGH")
-    registry.add_las_map("CALI", "CM", "centimeter", "HIGH")
-    registry.add_las_map("CALI", "IN", "inch", "HIGH")
-    registry.add_las_map("DRHO", "G/C3", "gram / centimeter ** 3", "HIGH")
-    registry.add_las_map("RHOB", "G/C3", "gram / centimeter ** 3", "HIGH")
-    registry.add_las_map("DT", "US/F", "microsecond / foot", "HIGH")
-    registry.add_las_map("ILD", "OHMM", "ohm * meter", "HIGH")
-    registry.add_las_map("LLS", "OHMM", "ohm * meter", "HIGH")
-    registry.add_las_map("ILM", "OHMM", "ohm * meter", "HIGH")
-    registry.add_las_map("LLD", "OHMM", "ohm * meter", "HIGH")
-    registry.add_las_map("SFL", "OHMM", "ohm * meter", "HIGH")
-    registry.add_las_map("SGR", "GAPI", "gAPI", "HIGH")
-    registry.add_las_map("SP", "MV", "millivolt", "HIGH")
-    registry.add_las_map("MSFL", "OHMM", "ohm * meter", "HIGH")
-    registry.add_las_map("NPHI", "PU", PU)
-    registry.add_las_map("NPHI", "PU%", "pu", "HIGH")
-    registry.add_las_map("PEF", "", percent_general)
-    registry.add_las_map("POTA", "", percent_general)
-    registry.add_las_map("THOR", "", percent_general)
-    registry.add_las_map("URAN", "", percent_general)
+def registry_mapping(las_map: LasMap) -> None:
+    for unit_args in units:
+        las_map.add_las_map(*unit_args)
