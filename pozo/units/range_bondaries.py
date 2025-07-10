4bc2fedd5f303438d26668ef7ab13d30eecc626c
David Angarita <davidangarita.sistemas@gmail.com>
2025-07-10 18:42:03 -0500

    Rename unit name from RangeBondaries

--- pozo/units/range_bondaries.py
+++ pozo/units/range_bondaries.py
@@ -1,12 +1,12 @@
 class RangeBoundaries:
-    def __init__(self, boundaries, unit: str | tuple["RangeBoundaries"], confidence):
+    def __init__(self, boundaries, las_unit: str | tuple["RangeBoundaries"], confidence):
         if not isinstance(boundaries, tuple) or len(boundaries) not in {0, 2}:
             raise TypeError(
                 "boundaries should contain a tuple with (min, max) or () catch-all"
             )
 
         self.boundaries = boundaries
-        self.unit = unit
+        self.unit = las_unit
         self.confidence = confidence
 
     def is_within_range(self, min_val, max_val):
