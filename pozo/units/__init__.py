c9b428820996b2acdbcba9092e880095ec678ae8
David Angarita <davidangarita.sistemas@gmail.com>
2025-07-11 18:31:18 -0500

    Rename display_utils to display

--- pozo/units/__init__.py
+++ pozo/units/__init__.py
@@ -8,7 +8,7 @@ from lasio import LASFile
 from pozo.units import errors
 from pozo.units.las_si import las_si_config, las_si_map
 from pozo.units.si_pint import si_pint_config
-from pozo.utils import _table_utils, stats, display_utils, lasio_utils, types
+from pozo.utils import _table_utils, display, lasio_utils, stats, types
 
 os.environ["PINT_ARRAY_PROTOCOL_FALLBACK"] = "0"  # from numpy/pint documentation
 
@@ -105,13 +105,11 @@ def check_las(las: LASFile, HTML_out=True, div_id="") -> None:
 
         try:
             html_output = _table_utils.generate_html_table(result, _delimiter)
-            display_utils.show_content(
-                f'<div id="{div_id}">{html_output}</div>', html=True
-            )
+            display.show_content(f'<div id="{div_id}">{html_output}</div>', html=True)
 
         except Exception as e:
-            display_utils.show_content(str(e))
-            display_utils.show_content("<br>".join(result), html=True)
+            display.show_content(str(e))
+            display.show_content("<br>".join(result), html=True)
 
 
 def parse_unit_safe(unit: str) -> pint.Unit | None:
