f8d378f810768b80f6b9e9a75f132261cd1f1d73
David Angarita <davidangarita.sistemas@gmail.com>
2025-07-15 16:32:00 -0500

    Create custom type

--- pozo/utils/types.py
+++ pozo/utils/types.py
@@ -1,12 +1,30 @@
-from typing import Any, Protocol, TypeAlias
+from __future__ import annotations
+
+from typing import Any, Protocol, TypeAlias, TypedDict
 
 Array: TypeAlias = Any  # temporal
 
 
 class Curve(Protocol):
+    """Protocol with curve properties."""
+
     @property
-    def mnemonic(self) -> str: ...
+    def mnemonic(self) -> str: ...  # noqa: D102
     @property
-    def unit(self) -> str: ...
+    def unit(self) -> str: ...  # noqa: D102
     @property
-    def data(self) -> Array | None: ...
+    def data(self) -> Array | None: ...  # noqa: D102
+
+
+class Diagnosis(TypedDict):
+    """Custon Dict type for diagnosis of las to si."""
+
+    mnemonic: str
+    las_unit: str
+    data: Array | None
+    confidence: str
+    si_unit: str
+    v_min: str
+    v_med: str
+    v_max: str
+    n_nan: int
