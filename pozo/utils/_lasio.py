d62bdc35bd5ed73bc043147144f914911279f615
David Angarita <davidangarita.sistemas@gmail.com>
2025-07-15 13:30:13 -0500

    Create new utility fo _lasio utils

--- pozo/utils/_lasio.py
+++ pozo/utils/_lasio.py
@@ -1,2 +1,11 @@
+import re
+
+
 def remove_lasio_suffix(mnemonic: str) -> str:
     return mnemonic.split(":", 1)[0] if ":" in mnemonic else mnemonic
+
+
+def remove_prefix_number(descr: str) -> str:
+    desc_wo_num = re.compile(r"^(?:\s*\d+\s+)?(.*)$")
+    desc_match = desc_wo_num.findall(descr)
+    return desc_match[0] if len(desc_match) > 0 else descr
