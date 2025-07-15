e29c71436275c2789735b6103439976d58162bde
David Angarita <davidangarita.sistemas@gmail.com>
2025-07-15 16:37:09 -0500

    Create las_to_si_diagnosis and las_to_si functions

--- pozo/units/las_si/mapper.py
+++ pozo/units/las_si/mapper.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from pozo.utils import _lasio as lasio_utils
-from pozo.utils import stats
+from pozo.utils import stats, types
 
 if TYPE_CHECKING:
     from typing import Any, TypeAlias
@@ -122,3 +122,41 @@ class LasSiMap:
             las_unit = self._si_to_las_by_mnemonic.get("-", {}).get(si_unit)
 
         return las_unit
+
+    def las_to_si_diagnosis(
+        self,
+        mnemonic: str,
+        las_unit: str,
+        data: list[Any],
+    ) -> types.Diagnosis:
+        """Convert a LAS unit to SI Diagnosis using mnemonic."""
+        _range = self._las_to_Range(mnemonic, las_unit, data)
+        [v_min, v_med, v_max] = stats.quantiles_values(data, [0, 0.5, 1])
+        n_nan = stats.count_missing_values(data)
+
+        if _range is not None:
+            si_unit = _range.unit
+            confidence = _range.confidence
+        else:
+            si_unit = ""
+            confidence = (
+                f"- {las_unit} for {lasio_utils.remove_lasio_suffix(mnemonic)} - 0%"
+            )
+
+        diagnosis = types.Diagnosis(
+            mnemonic=mnemonic,
+            las_unit=las_unit,
+            data=data,
+            si_unit=si_unit,
+            confidence=confidence,
+            v_min=v_min,
+            v_med=v_med,
+            v_max=v_max,
+            n_nan=n_nan,
+        )
+        return diagnosis
+
+    def las_to_si(self, mnemonic: str, las_unit: str, data: list[Any]) -> str:
+        """Convert a LAS unit to SI using mnemonic and diagnosis."""
+        si_dict = self.las_to_si_diagnosis(mnemonic, las_unit, data)
+        return si_dict["si_unit"]
