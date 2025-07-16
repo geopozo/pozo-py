23f7898c1f9d1119ba04822fa9ef76cc2eab7449
David Angarita <davidangarita.sistemas@gmail.com>
2025-07-16 14:06:36 -0500

    Add docstring of display utils

--- pozo/utils/display.py
+++ pozo/utils/display.py
@@ -1,5 +1,8 @@
+"""Display util functions."""
+
 from IPython.display import HTML, display
 
 
-def show_content(content: str, *, html=False):
-    return display(HTML(content) if html else content)
+def show_content(content: str, *, html: bool = False) -> None:
+    """Display text or HTML content in an IPython environment."""
+    display(HTML(content) if html else content)
