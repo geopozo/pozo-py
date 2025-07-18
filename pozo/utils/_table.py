35bce8a40006718e76c268e7cc47013a2a90beb9
David Angarita <davidangarita.sistemas@gmail.com>
2025-07-18 11:01:28 -0500

    Make private apply color styling

--- pozo/utils/_table.py
+++ pozo/utils/_table.py
@@ -3,7 +3,7 @@ import re
 from pozo.utils import _stats
 
 
-def apply_color_styling(
+def _apply_color_styling(
     html_str: str,
     pattern: re.Pattern,
     color: str,
@@ -25,7 +25,7 @@ def generate_html_table(
 
     output = _stats.format_csv(post_result, delimiter)
     html_output = output.to_html()
-    html_output = apply_color_styling(html_output, red_low, "red")
-    html_output = apply_color_styling(html_output, orange_medium, "#B95000")
+    html_output = _apply_color_styling(html_output, red_low, "red")
+    html_output = _apply_color_styling(html_output, orange_medium, "#B95000")
 
     return html_output
