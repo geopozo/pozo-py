3a9902fa4f80c8d227b37f24f3f9c7a54fbac6a6
David Angarita <davidangarita.sistemas@gmail.com>
2025-07-22 15:52:13 -0500

    Fix import utils

--- src/pozo/graphs.py
+++ src/pozo/graphs.py
@@ -8,7 +8,7 @@ import pozo
 import pozo.renderers as pzr
 import pozo.themes as pzt
 import pozo.units as pzu
-from pozo.utils import _lasio as lasio_utils
+from pozo._utils import _lasio as lasio_utils
 
 desc_wo_num = re.compile(r"^(?:\s*\d+\s+)?(.*)$")
 LAS_TYPE = "<class 'lasio.las.LASFile'>"
