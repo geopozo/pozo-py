816ab1b5280d8a793c3b2e8afb4916a707dcf55f
David Angarita <davidangarita.sistemas@gmail.com>
2025-07-25 14:35:39 -0500

    Remove confidence arg, too many args

--- tests/units/las_si/test_mapper.py
+++ tests/units/las_si/test_mapper.py
@@ -5,13 +5,13 @@ from pozo.units.las_si.mapper import Range
 
 class TestRange:
     @pytest.mark.parametrize(
-        ("unit", "boundaries", "min_val", "max_val", "confidence", "expected"),
+        ("unit", "boundaries", "min_val", "max_val", "expected"),
         [
-            ("of_1", (-0.5, 0.8), 0.2, 0.7, "decide by range", True),
-            ("of_1", (-0.5, 0.8), -0.5, 0.8, "decide by range", False),
-            ("pu", (-50, 80), 0, 79.9, "catch all, legacy unit", True),
-            ("pu", (-50, 80), -100, 100, "decide by range, verify", False),
-            ("puAPI", (), -1000, 1000, "catch all, ppm", True),
+            ("of_1", (-0.5, 0.8), 0.2, 0.7, True),
+            ("of_1", (-0.5, 0.8), -0.5, 0.8, False),
+            ("pu", (-50, 80), 0, 79.9, True),
+            ("pu", (-50, 80), -100, 100, False),
+            ("puAPI", (), -1000, 1000, True),
         ],
     )
     def test_is_within_range(self, unit, boundaries, min_val, max_val, expected):
