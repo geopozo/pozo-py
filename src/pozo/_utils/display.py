c3ae463e61db74d3e428738307171753e650bc9b
David Angarita <davidangarita.sistemas@gmail.com>
2025-08-11 15:36:58 -0500

    Add conditional for show_html

--- src/pozo/_utils/display.py
+++ src/pozo/_utils/display.py
@@ -10,7 +10,7 @@ from IPython.display import HTML, display
 
 def show_html(content: str, *, html: bool = False) -> mo.Html | None:
     """Display text or HTML content in an IPython environment."""
-    if "marimo" in sys.modules:
+    if "marimo" in sys.modules and "ipykernel" not in sys.modules:
         return mo.Html(content)
     else:
         display(HTML(content) if html else content)
