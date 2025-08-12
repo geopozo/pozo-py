6f635a0c298ab9ab246becb598dd55ad91e2bda1
David Angarita <davidangarita.sistemas@gmail.com>
2025-08-12 13:19:28 -0500

    Modify test_table tests

--- tests/utils/test_table.py
+++ tests/utils/test_table.py
@@ -1,54 +1,31 @@
+import json
+from pathlib import Path
+
 import pytest
 
 from pozo._utils import table
 
+route = Path(__file__).parent / "table_data.json"
+with route.open("r", encoding="utf-8") as file:
+    json_data = json.load(file)
+
 
 class TestGenerateHtmlTable:
     @pytest.mark.parametrize(
-        ("confidence", "expected"),
-        [
-            (10, "style='color:red'"),
-            (33, "style='color:red'"),
-            (34, "style='color:#B95000'"),
-            (66, "style='color:#B95000'"),
-            (67, ""),
-            (100, ""),
-        ],
+        ("data", "expected", "n_rows", "style"),
+        list(
+            zip(
+                json_data["input"],
+                json_data["output"],
+                json_data["n_rows"],
+                json_data["styles"],
+            ),
+        ),
+        ids=json_data["tests"],
     )
-    def test_generate_html_table_confidence_styles(self, confidence, expected):
-        data = ["label,confidence,min", f"A,{confidence},9.99"]
-        delimiter = ","
-
-        result = table.generate_html_table(data, delimiter)
-
-        expected_cell = f"<td{f' {expected}' if expected else ''}>{confidence}</td>"
-        assert expected_cell in result
-
-    def test_generate_html_table_basic_structure(self):
-        data = ["mnemonic,las unit,confidence", "DEFT,M,100"]
-        delimiter = ","
-
-        result = table.generate_html_table(data, delimiter)
-
-        assert result.startswith("<table border='1'>")
-        assert result.endswith("</table>")
-        assert "<thead>" in result
-        assert "</thead>" in result
-        assert "<tbody>" in result
-        assert "</tbody>" in result
-
-    def test_generate_html_table_multiple_rows(self):
-        data = [
-            "mnemonic,las_unit,confidence",
-            "DEPT,M,100",
-            "ILD,OHMM,100",
-            "GR,GAPI,100",
-        ]
-        delimiter = ","
-
-        result = table.generate_html_table(data, delimiter)
-
-        assert result.count("<tr>") == 4
-        assert "<td>DEPT</td>" in result
-        assert "<td>ILD</td>" in result
-        assert "<td>GR</td>" in result
+    def test_generate_html_table_confidence_styles(self, data, expected, n_rows, style):
+        result = table.generate_html_table(data)
+        print(result)
+        assert result in expected
+        assert result.count("<tr") is n_rows
+        assert style in result
